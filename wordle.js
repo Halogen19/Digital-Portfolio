e9b998a378dcd97095172cfcdb520f4ecbeb51f6
Halogen19 <Hamish.nye@gmail.com>
2023-11-10 20:13:12 +0000

    filling up next available box with value of key clicked, and getting back button working

--- wordle.js
+++ wordle.js
@@ -1,6 +1,7 @@
 const keyCharacters = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','ENTER','Z','X','C','V','B','N','M','BACK']
 const grid = document.querySelector("#wordleGrid")
 const keyboard = document.querySelector("#keyboardBox")
+var layer = 1;
 
 
 function createGrid(){
@@ -20,10 +21,30 @@ function createKeyboard(){
         key.append(keyCharacters[i])
         keyboard.append(key)
         key.addEventListener('click', function(){
+            //function occurs when any key on created keyboard is pressed
+            //debugging line
             console.log('Key ' + (i+1) + ' was clicked!');
-            document.getElementById('square1').innerHTML = "";
-            document.getElementById('square1').append(key.getAttribute('value'));
-            
+
+
+            //stops enter and back keys appearing in boxes when they are clicked
+            if (key.getAttribute('value') != "BACK" && key.getAttribute('value') != "ENTER"){
+                //adds letters to box divs allowing the user to write words
+                for (let j = 0; j <30;j++){
+                    if (document.getElementById('square' +(j+1)).innerHTML === ""){
+                        document.getElementById('square' +(j+1)).append(key.getAttribute('value'));
+                        break;
+                    }
+                }
+            }else if (key.getAttribute('value') == "BACK" && document.getElementById('square1').innerHTML != ""){
+                for (j = 0;j<30;j++){
+                    if(document.getElementById('square'+(j+1)).innerHTML === ""){
+                        document.getElementById('square'+(j)).innerHTML = ""
+                    }
+                }
+                if(document.getElementById('square30').innerHTML != ""){
+                    document.getElementById('square30').innerHTML = ""
+                }
+            }
         });
     }
 }
