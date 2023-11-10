2cc792dba10b5c096c9754e3046514e15b8412a3
Halogen19 <Hamish.nye@gmail.com>
2023-11-10 17:41:12 +0000

    added blank keyboard key divs and sorted keyboard layout

--- wordle.js
+++ wordle.js
@@ -9,4 +9,14 @@ function createGrid(){
         grid.append(square)
     }
 }
+function createKeyboard(){
+    for (let i = 0; i<28;i++){
+        const key = document.createElement('div')
+        key.classList.add('key')
+        key.setAttribute('id','key' + (i+1))
+        keyboard.append(key)
+    }
+
+}
 createGrid()
+createKeyboard()
